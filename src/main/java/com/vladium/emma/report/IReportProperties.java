jiangxincode/Emma

commit 42400ef32d3644fe1632f4c0f3f9d299c49c6bc6

fixed the source code encoding issue

src/main/java/com/vladium/emma/report/IReportProperties.java
@@ -68,6 +68,10 @@ interface IReportProperties
     String METRICS          = "metrics"; // comma-separated list of (column name:metric) pairs
     char MSEPARATOR         = ':';
 
+    // addition parameters by terry
+    String OUTPUT_ENCODING = "html.outputEncoding";
+    String SOURCE_ENCODING = "html.sourceEncoding";
+
     // defaults:
     
     String DEFAULT_UNITS_TYPE = INSTR_UNITS;

src/main/java/com/vladium/emma/report/ReportProperties.java
@@ -38,18 +38,6 @@ abstract class ReportProperties implements IAppErrorCodes
        
     public static final class ParsedProperties
     {
-        public void setOutEncoding (final String outEncoding)
-        {
-            if ($assert.ENABLED) $assert.ASSERT (outEncoding != null, "null input: outEncoding");
-            
-            m_outEncoding = outEncoding;
-        }
-
-        public String getOutEncoding ()
-        {
-            return m_outEncoding;
-        }
-
         public void setOutDir (final File outDir)
         {
             if ($assert.ENABLED) $assert.ASSERT (outDir != null, "null input: outDir");
@@ -167,6 +155,8 @@ abstract class ReportProperties implements IAppErrorCodes
                 $assert.ASSERT (m_columnOrder != null, "m_columnOrder not set");
                 $assert.ASSERT (m_sortOrder != null, "m_sortOrder not set");
                 $assert.ASSERT (m_metrics != null, "m_metrics not set");
+                $assert.ASSERT (m_sourceEncoding != null, "m_sourceEncoding not set");
+                $assert.ASSERT (m_outputEncoding != null, "m_outputEncoding not set");
             }
         }
 
@@ -186,7 +176,28 @@ abstract class ReportProperties implements IAppErrorCodes
         private int [] m_columnOrder; // attribute IDs [order indicates column order] 
         private int [] m_sortOrder; // if m_sortOrder[i+1]>0 , sort m_columnOrder[m_sortOrder[i]] in ascending order
         private IntIntMap m_metrics; // pass criteria (column attribute ID -> metric)
-        
+
+        // addition parameters by terry
+        private String m_outputEncoding;
+        private String m_sourceEncoding;
+
+        public String getSourceEncoding() {
+            return m_sourceEncoding;
+        }
+
+        public void setSourceEncoding(String sourceEncoding) {
+            if ($assert.ENABLED) $assert.ASSERT (sourceEncoding != null, "null input: sourceEncoding");
+            this.m_sourceEncoding = sourceEncoding;
+        }
+
+        public String getOutputEncoding() {
+            return m_outputEncoding;
+        }
+
+        public void setOutputEncoding(String outputEncoding) {
+            if ($assert.ENABLED) $assert.ASSERT (outputEncoding != null, "null input: outputEncoding");
+            this.m_outputEncoding = outputEncoding;
+        }
     } // end of nested class
     
     
@@ -245,7 +256,8 @@ abstract class ReportProperties implements IAppErrorCodes
         
         final ParsedProperties result = new ParsedProperties ();
         {
-            result.setOutEncoding (getReportProperty (properties, type, IReportProperties.OUT_ENCODING, false, false));
+            result.setOutputEncoding(getReportProperty (properties, type, IReportProperties.OUTPUT_ENCODING, false, false));
+            result.setSourceEncoding(getReportProperty (properties, type, IReportProperties.SOURCE_ENCODING, false, false));
         }
         
         // TODO: outDirName is no longer supported

src/main/java/com/vladium/emma/report/html/ReportGenerator.java
@@ -8,14 +8,7 @@
  */
 package com.vladium.emma.report.html;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.text.DecimalFormat;
 import java.text.FieldPosition;
 import java.text.NumberFormat;
@@ -148,7 +141,7 @@ final class ReportGenerator extends AbstractReportGenerator
             
             m_log.info ("writing [" + getType () + "] report to [" + fullOutFile.getAbsolutePath () + "] ...");
             
-            out = openOutFile (fullOutFile, m_settings.getOutEncoding (), true);
+            out = openOutFile (fullOutFile, m_settings.getOutputEncoding(), true);
             
             final int [] columns = m_settings.getColumnOrder ();
             final StringBuffer buf = new StringBuffer ();
@@ -326,7 +319,7 @@ final class ReportGenerator extends AbstractReportGenerator
             
             final File outFile = getItemFile (NESTED_ITEMS_PARENT_DIR, m_reportIDNamespace.getID (getItemKey (item)));
             
-            out = openOutFile (Files.newFile (m_settings.getOutDir (), outFile), m_settings.getOutEncoding (), true);
+            out = openOutFile (Files.newFile (m_settings.getOutDir (), outFile), m_settings.getOutputEncoding (), true);
             
             final int [] columns = m_settings.getColumnOrder ();            
             final StringBuffer buf = new StringBuffer ();
@@ -442,86 +435,86 @@ final class ReportGenerator extends AbstractReportGenerator
         try
         {
             final File outFile = getItemFile (NESTED_ITEMS_PARENT_DIR, m_reportIDNamespace.getID (getItemKey (item)));
-            
-            out = openOutFile (Files.newFile (m_settings.getOutDir (), outFile), m_settings.getOutEncoding (), true);
-            
-            final int [] columns = m_settings.getColumnOrder ();            
+
+            out = openOutFile (Files.newFile (m_settings.getOutDir (), outFile), m_settings.getOutputEncoding (), true);
+
+            final int [] columns = m_settings.getColumnOrder ();
             final StringBuffer buf = new StringBuffer ();
-            
+
             // TODO: set title [from a prop?]
             final HTMLDocument page = createPage (REPORT_HEADER_TITLE);
             {
                 final IItem [] path = getParentPath (item);
-                
+
                 addPageHeader (page, item, path);
                 addPageFooter (page, item, path);
             }
-            
+
             // summary table:
-            
+
             {
                 final IElement itemname = IElement.Factory.create (Tag.SPAN);
                 itemname.setText (item.getName (), true);
                 itemname.setClass (CSS_ITEM_NAME);
-                
+
                 final IElementList title = new ElementList ();
                 title.add (new Text ("COVERAGE SUMMARY FOR SOURCE FILE [", true));
                 title.add (itemname);
                 title.add (new Text ("]", true));
-                
+
                 page.addH (1, title, null);
             }
-            
+
             final HTMLTable summaryTable = new HTMLTable ("100%", null, null, "0");
             {
                 // header row:
                 final HTMLTable.IRow header = summaryTable.newTitleRow ();
                 // coverage row:
                 final HTMLTable.IRow coverage = summaryTable.newRow ();
-                
+
                 for (int c = 0; c < columns.length; ++ c)
                 {
                     final int attrID = columns [c];
                     final IItemAttribute attr = item.getAttribute (attrID, m_settings.getUnitsType ());
-                    
+
                     final HTMLTable.ICell headercell = header.newCell ();
                     headercell.setText (attr.getName (), true);
-                    
+
                     if (attr != null)
                     {
                         boolean fail = (m_metrics [attrID] > 0) && ! attr.passes (item, m_metrics [attrID]);
-                        
+
                         buf.setLength (0);
                         attr.format (item, buf);
-                        
-                        final HTMLTable.ICell cell = coverage.newCell (); 
+
+                        final HTMLTable.ICell cell = coverage.newCell ();
                         cell.setText (buf.toString (), true);
                         if (fail) cell.setClass (CSS_DATA_HIGHLIGHT);
                     }
                 }
             }
-            page.add (summaryTable);    
-            
+            page.add (summaryTable);
+
             final boolean deeper = (m_settings.getDepth () > ClassItem.getTypeMetadata ().getTypeID ());
             final boolean embedSrcFile = deeper && srcFileAvailable (item, m_cache);
             final boolean createAnchors = embedSrcFile && m_hasLineNumberInfo;
-            
+
             final IDGenerator pageIDNamespace = createAnchors ? new IDGenerator () : null;
-            
+
             // child summary table is special for srcfile items:
-            
+
             page.addH (2, "COVERAGE BREAKDOWN BY CLASS AND METHOD", null);
 
             final IntObjectMap lineAnchorIDMap = embedSrcFile ? new IntObjectMap () : null;
             final HTMLTable childSummaryTable = new HTMLTable ("100%", null, null, "0");
-            
+
             childSummaryTable.setClass (CSS_CLS_NOLEFT);
-            
+
             {
                 int [] headerColumns = null;
 
                 final ItemComparator order = m_typeSortComparators [ClassItem.getTypeMetadata ().getTypeID ()];
-                int clsIndex = 0;                
+                int clsIndex = 0;
                 for (Iterator classes = item.getChildren (order); classes.hasNext (); ++ clsIndex)
                 {
                     final ClassItem cls = (ClassItem) classes.next ();
@@ -529,11 +522,11 @@ final class ReportGenerator extends AbstractReportGenerator
                     if (headerColumns == null)
                     {
                         // header row:
-                        headerColumns = addHeaderRow (cls, childSummaryTable, columns);                        
+                        headerColumns = addHeaderRow (cls, childSummaryTable, columns);
                     }
-                    
+
                     String HREFname = null;
-                    
+
                     // special class subheader:
                     if (createAnchors)
                     {
@@ -542,28 +535,28 @@ final class ReportGenerator extends AbstractReportGenerator
                             $assert.ASSERT (lineAnchorIDMap != null);
                             $assert.ASSERT (pageIDNamespace != null);
                         }
-                        
+
                         final String childKey = getItemKey (cls);
-                        
+
                         HREFname = addLineAnchorID (cls.getFirstLine (), pageIDNamespace.getID (childKey), lineAnchorIDMap);
                     }
 
                     addClassRow (cls, clsIndex, childSummaryTable, headerColumns, HREFname, createAnchors);
-                    
+
 //                    // row to separate this class's methods:
 //                    final HTMLTable.IRow subheader = childSummaryTable.newTitleRow ();
 //                    final HTMLTable.ICell cell = subheader.newCell ();
 //                    // TODO: cell.setColspan (???)
 //                    cell.setText ("class " + child.getName () + " methods:", true);
-                    
+
                     boolean odd = false;
-                    final ItemComparator order2 = m_typeSortComparators [MethodItem.getTypeMetadata ().getTypeID ()];                
+                    final ItemComparator order2 = m_typeSortComparators [MethodItem.getTypeMetadata ().getTypeID ()];
                     for (Iterator methods = cls.getChildren (order2); methods.hasNext (); odd = ! odd)
                     {
                         final MethodItem method = (MethodItem) methods.next ();
-                        
+
                         HREFname = null;
-                        
+
                         if (createAnchors)
                         {
                             if ($assert.ENABLED)
@@ -571,9 +564,9 @@ final class ReportGenerator extends AbstractReportGenerator
                                 $assert.ASSERT (lineAnchorIDMap != null);
                                 $assert.ASSERT (pageIDNamespace != null);
                             }
-                            
+
                             final String child2Key = getItemKey (method);
-                            
+
                             HREFname = addLineAnchorID (method.getFirstLine (), pageIDNamespace.getID (child2Key), lineAnchorIDMap);
                         }
 
@@ -582,10 +575,10 @@ final class ReportGenerator extends AbstractReportGenerator
                 }
             }
             page.add (childSummaryTable);
-            
-            
+
+
             // embed source file:
-             
+
             if (deeper)
             {
                 //page.addHR (1);
@@ -595,9 +588,9 @@ final class ReportGenerator extends AbstractReportGenerator
                 }
                 //page.addHR (1);
             }
-                
-            
-            page.emit (out);            
+
+
+            page.emit (out);
             out.flush ();
         }
         finally
@@ -619,7 +612,7 @@ final class ReportGenerator extends AbstractReportGenerator
             final File outFile = getItemFile (NESTED_ITEMS_PARENT_DIR, m_reportIDNamespace.getID (getItemKey (item)));
             
             // TODO: deal with overwrites
-            out = openOutFile (Files.newFile (m_settings.getOutDir (), outFile), m_settings.getOutEncoding (), true);
+            out = openOutFile (Files.newFile (m_settings.getOutDir (), outFile), m_settings.getOutputEncoding (), true);
             
             final int [] columns = m_settings.getColumnOrder ();            
             final StringBuffer buf = new StringBuffer ();
@@ -763,7 +756,7 @@ final class ReportGenerator extends AbstractReportGenerator
     
     private HTMLDocument createPage (final String title)
     {
-        final HTMLDocument page = new HTMLDocument (title, m_settings.getOutEncoding ());
+        final HTMLDocument page = new HTMLDocument (title, m_settings.getOutputEncoding ());
         page.addStyle (CSS); // TODO: split by visit type
         
         return page;
@@ -1142,8 +1135,8 @@ final class ReportGenerator extends AbstractReportGenerator
                 BufferedReader in = null;
                 try
                 {
-                    in = new BufferedReader (new FileReader (srcFile), IO_BUF_SIZE);
-                    
+                    in = new BufferedReader (new InputStreamReader (new FileInputStream(srcFile), m_settings.getSourceEncoding ()), IO_BUF_SIZE);
+
                     final boolean markupCoverage = m_hasLineNumberInfo;
                     
                     final int unitsType = m_settings.getUnitsType ();

src/main/java/com/vladium/emma/report/txt/ReportGenerator.java
@@ -113,7 +113,7 @@ final class ReportGenerator extends AbstractReportGenerator
         
         m_log.info ("writing [" + getType () + "] report to [" + fullOutFile.getAbsolutePath () + "] ...");
         
-        openOutFile (fullOutFile, m_settings.getOutEncoding (), true);
+        openOutFile (fullOutFile, m_settings.getOutputEncoding (), true);
         
         // build ID stamp:
         try

src/main/java/com/vladium/emma/report/xml/ReportGenerator.java
@@ -106,10 +106,10 @@ final class ReportGenerator extends AbstractReportGenerator
             
             m_log.info ("writing [" + getType () + "] report to [" + fullOutFile.getAbsolutePath () + "] ...");
             
-            openOutFile (fullOutFile, m_settings.getOutEncoding (), true);
+            openOutFile (fullOutFile, m_settings.getOutputEncoding (), true);
             
             // XML header:
-            m_out.write ("<?xml version=\"1.0\" encoding=\"" + m_settings.getOutEncoding () + "\"?>");
+            m_out.write ("<?xml version=\"1.0\" encoding=\"" + m_settings.getOutputEncoding () + "\"?>");
             
             // build ID stamp:
             try

commit 6e2378b3439f58a2e1ddb7b33e45b458415562e1

fix the bug introduced by matianyi in 42400ef32d3644fe1632f4c0f3f9d299c49c6bc6

src/main/java/com/vladium/emma/report/IReportProperties.java
@@ -22,6 +22,9 @@ interface IReportProperties
     String PREFIX = "report.";
     
     // parameter:
+    String IN_ENCODING = "in.encoding";
+    
+    // parameter:
     String OUT_ENCODING     = "out.encoding";
     String OUT_DIR          = "out.dir";
     String OUT_FILE         = "out.file";
@@ -67,10 +70,6 @@ interface IReportProperties
     // parameter:
     String METRICS          = "metrics"; // comma-separated list of (column name:metric) pairs
     char MSEPARATOR         = ':';
-
-    // addition parameters by terry
-    String OUTPUT_ENCODING = "html.outputEncoding";
-    String SOURCE_ENCODING = "html.sourceEncoding";
 
     // defaults:
     

src/main/java/com/vladium/emma/report/ReportProperties.java
@@ -38,6 +38,30 @@ abstract class ReportProperties implements IAppErrorCodes
        
     public static final class ParsedProperties
     {
+        public void setInEncoding (final String inEncoding)
+        {
+            if ($assert.ENABLED) $assert.ASSERT (inEncoding != null, "null input: inEncoding");
+            
+            m_inEncoding = inEncoding;
+        }
+
+        public String getInEncoding ()
+        {
+            return m_inEncoding;
+        }
+        
+        public void setOutEncoding (final String outEncoding)
+        {
+            if ($assert.ENABLED) $assert.ASSERT (outEncoding != null, "null input: outEncoding");
+            
+            m_outEncoding = outEncoding;
+        }
+
+        public String getOutEncoding ()
+        {
+            return m_outEncoding;
+        }
+        
         public void setOutDir (final File outDir)
         {
             if ($assert.ENABLED) $assert.ASSERT (outDir != null, "null input: outDir");
@@ -155,12 +179,10 @@ abstract class ReportProperties implements IAppErrorCodes
                 $assert.ASSERT (m_columnOrder != null, "m_columnOrder not set");
                 $assert.ASSERT (m_sortOrder != null, "m_sortOrder not set");
                 $assert.ASSERT (m_metrics != null, "m_metrics not set");
-                $assert.ASSERT (m_sourceEncoding != null, "m_sourceEncoding not set");
-                $assert.ASSERT (m_outputEncoding != null, "m_outputEncoding not set");
             }
         }
 
-
+        private String m_inEncoding;
         private String m_outEncoding;
         private File m_outDir;
         private File m_outFile;
@@ -176,28 +198,6 @@ abstract class ReportProperties implements IAppErrorCodes
         private int [] m_columnOrder; // attribute IDs [order indicates column order] 
         private int [] m_sortOrder; // if m_sortOrder[i+1]>0 , sort m_columnOrder[m_sortOrder[i]] in ascending order
         private IntIntMap m_metrics; // pass criteria (column attribute ID -> metric)
-
-        // addition parameters by terry
-        private String m_outputEncoding;
-        private String m_sourceEncoding;
-
-        public String getSourceEncoding() {
-            return m_sourceEncoding;
-        }
-
-        public void setSourceEncoding(String sourceEncoding) {
-            if ($assert.ENABLED) $assert.ASSERT (sourceEncoding != null, "null input: sourceEncoding");
-            this.m_sourceEncoding = sourceEncoding;
-        }
-
-        public String getOutputEncoding() {
-            return m_outputEncoding;
-        }
-
-        public void setOutputEncoding(String outputEncoding) {
-            if ($assert.ENABLED) $assert.ASSERT (outputEncoding != null, "null input: outputEncoding");
-            this.m_outputEncoding = outputEncoding;
-        }
     } // end of nested class
     
     
@@ -256,8 +256,8 @@ abstract class ReportProperties implements IAppErrorCodes
         
         final ParsedProperties result = new ParsedProperties ();
         {
-            result.setOutputEncoding(getReportProperty (properties, type, IReportProperties.OUTPUT_ENCODING, false, false));
-            result.setSourceEncoding(getReportProperty (properties, type, IReportProperties.SOURCE_ENCODING, false, false));
+            result.setOutEncoding(getReportProperty (properties, type, IReportProperties.OUT_ENCODING, false, false));
+            result.setInEncoding(getReportProperty (properties, type, IReportProperties.IN_ENCODING, false, false));
         }
         
         // TODO: outDirName is no longer supported

src/main/java/com/vladium/emma/report/html/ReportGenerator.java
@@ -141,7 +141,7 @@ final class ReportGenerator extends AbstractReportGenerator
             
             m_log.info ("writing [" + getType () + "] report to [" + fullOutFile.getAbsolutePath () + "] ...");
             
-            out = openOutFile (fullOutFile, m_settings.getOutputEncoding(), true);
+            out = openOutFile (fullOutFile, m_settings.getOutEncoding(), true);
             
             final int [] columns = m_settings.getColumnOrder ();
             final StringBuffer buf = new StringBuffer ();
@@ -319,7 +319,7 @@ final class ReportGenerator extends AbstractReportGenerator
             
             final File outFile = getItemFile (NESTED_ITEMS_PARENT_DIR, m_reportIDNamespace.getID (getItemKey (item)));
             
-            out = openOutFile (Files.newFile (m_settings.getOutDir (), outFile), m_settings.getOutputEncoding (), true);
+            out = openOutFile (Files.newFile (m_settings.getOutDir (), outFile), m_settings.getOutEncoding (), true);
             
             final int [] columns = m_settings.getColumnOrder ();            
             final StringBuffer buf = new StringBuffer ();
@@ -436,7 +436,7 @@ final class ReportGenerator extends AbstractReportGenerator
         {
             final File outFile = getItemFile (NESTED_ITEMS_PARENT_DIR, m_reportIDNamespace.getID (getItemKey (item)));
 
-            out = openOutFile (Files.newFile (m_settings.getOutDir (), outFile), m_settings.getOutputEncoding (), true);
+            out = openOutFile (Files.newFile (m_settings.getOutDir (), outFile), m_settings.getOutEncoding (), true);
 
             final int [] columns = m_settings.getColumnOrder ();
             final StringBuffer buf = new StringBuffer ();
@@ -612,7 +612,7 @@ final class ReportGenerator extends AbstractReportGenerator
             final File outFile = getItemFile (NESTED_ITEMS_PARENT_DIR, m_reportIDNamespace.getID (getItemKey (item)));
             
             // TODO: deal with overwrites
-            out = openOutFile (Files.newFile (m_settings.getOutDir (), outFile), m_settings.getOutputEncoding (), true);
+            out = openOutFile (Files.newFile (m_settings.getOutDir (), outFile), m_settings.getOutEncoding (), true);
             
             final int [] columns = m_settings.getColumnOrder ();            
             final StringBuffer buf = new StringBuffer ();
@@ -756,7 +756,7 @@ final class ReportGenerator extends AbstractReportGenerator
     
     private HTMLDocument createPage (final String title)
     {
-        final HTMLDocument page = new HTMLDocument (title, m_settings.getOutputEncoding ());
+        final HTMLDocument page = new HTMLDocument (title, m_settings.getOutEncoding ());
         page.addStyle (CSS); // TODO: split by visit type
         
         return page;
@@ -1135,7 +1135,7 @@ final class ReportGenerator extends AbstractReportGenerator
                 BufferedReader in = null;
                 try
                 {
-                    in = new BufferedReader (new InputStreamReader (new FileInputStream(srcFile), m_settings.getSourceEncoding ()), IO_BUF_SIZE);
+                    in = new BufferedReader (new InputStreamReader (new FileInputStream(srcFile), m_settings.getInEncoding ()), IO_BUF_SIZE);
 
                     final boolean markupCoverage = m_hasLineNumberInfo;
                     

src/main/java/com/vladium/emma/report/txt/ReportGenerator.java
@@ -113,7 +113,7 @@ final class ReportGenerator extends AbstractReportGenerator
         
         m_log.info ("writing [" + getType () + "] report to [" + fullOutFile.getAbsolutePath () + "] ...");
         
-        openOutFile (fullOutFile, m_settings.getOutputEncoding (), true);
+        openOutFile (fullOutFile, m_settings.getOutEncoding (), true);
         
         // build ID stamp:
         try

src/main/java/com/vladium/emma/report/xml/ReportGenerator.java
@@ -106,10 +106,10 @@ final class ReportGenerator extends AbstractReportGenerator
             
             m_log.info ("writing [" + getType () + "] report to [" + fullOutFile.getAbsolutePath () + "] ...");
             
-            openOutFile (fullOutFile, m_settings.getOutputEncoding (), true);
+            openOutFile (fullOutFile, m_settings.getOutEncoding (), true);
             
             // XML header:
-            m_out.write ("<?xml version=\"1.0\" encoding=\"" + m_settings.getOutputEncoding () + "\"?>");
+            m_out.write ("<?xml version=\"1.0\" encoding=\"" + m_settings.getOutEncoding () + "\"?>");
             
             // build ID stamp:
             try